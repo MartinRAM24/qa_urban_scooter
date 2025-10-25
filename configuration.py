0ac54a7e921391cabde50bdffffc141bcf0b7dce
Martin Ricardo Avalos Mendoza <martinquetza@gmail.com>
2025-10-25 16:32:48 -0600

    listo

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-URL_SERVICE = "https://cnt-7733d4be-4eb4-4e55-a270-e5279dcd9394.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-7b414fac-af99-4207-890a-fcf77090ba99.containerhub.tripleten-services.com"
 
 CREATE_COURIER = "/api/v1/courier"
 LOGIN_COURIER  = "/api/v1/courier/login"
--- test_create_user.py
+++ test_create_user.py
@@ -1,4 +1,4 @@
-import sender_stan_request as api
+import sender_stand_request as api
 import data
 
 def get_courier_id(login: str, password: str) -> int:
@@ -6,13 +6,6 @@ def get_courier_id(login: str, password: str) -> int:
     resp = api.post_login_courier(login_body)
     assert resp.status_code == 200, f"Login falló: {resp.status_code} {resp.text}"
     return resp.json()["id"]
-
-def get_delete_courier(login: str, password: str):
-    courier_id = get_courier_id(login, password)
-    delete_body = data.id_delivery.copy()
-    delete_body["id"] = str(courier_id)
-    resp_delete = api.delete_courier(delete_body, courier_id)
-    return resp_delete
 
 def positive_assert_create(login, password, first_name):
     reg_body = data.make_register_body(login, password, first_name)
@@ -24,18 +17,27 @@ def positive_assert_login(login, password):
     resp = api.post_login_courier(login_body)
     assert resp.status_code == 200
 
+def positive_delete_courier(login: str, password: str):
+    courier_id = get_courier_id(login, password)
+    delete_body = data.id_delivery.copy()
+    delete_body["id"] = str(courier_id)
+    resp_delete = api.delete_courier(delete_body, courier_id)
+    assert resp_delete.status_code == 200
+    resp_check = api.delete_courier(delete_body, courier_id)
+    assert resp_check.status_code == 404
+
+
 def test_create_user_4_chars():
     login = "abcd"
     password = "1234"
     positive_assert_create(login, password, "Juan")
     positive_assert_login(login, password)
-    r = get_delete_courier(login, password)
-    assert r.status_code in (200, 204)
+    positive_delete_courier(login, password)
+
 
 def test_create_user_10_chars():
     login = "abcdefghij"
     password = "1234"
     positive_assert_create(login, password, "Pedro")
     positive_assert_login(login, password)
-    r = get_delete_courier(login, password)
-    assert r.status_code in (200, 204)
+    positive_delete_courier(login, password)
